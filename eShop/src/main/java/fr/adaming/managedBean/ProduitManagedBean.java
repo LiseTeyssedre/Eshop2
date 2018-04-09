e179694a4da948e85dd5c0ff7c0ceb95ed432bc5
inti0423 <inti0423@DESKTOP-U3KTBH5>
2018-04-09 17:05:35 +0200

    gwe: ajouter produit

--- eShop/src/main/java/fr/adaming/managedBean/ProduitManagedBean.java
+++ eShop/src/main/java/fr/adaming/managedBean/ProduitManagedBean.java
@@ -4,12 +4,16 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
 
+import org.primefaces.model.UploadedFile;
+
+import fr.adaming.model.Categorie;
 import fr.adaming.model.Produit;
 import fr.adaming.service.IProduitService;
 
@@ -30,6 +34,8 @@ public class ProduitManagedBean implements Serializable{
 	private Produit produit;
 	private List<Produit> listeProduits;
 	private HttpSession maSession;
+	private UploadedFile uf;
+	private Categorie categorie;
 
 	//Constructeur vide
 	public ProduitManagedBean() {
@@ -39,7 +45,7 @@ public class ProduitManagedBean implements Serializable{
 	@PostConstruct
 	public void init(){
 		this.listeProduits = produitService.getAllProduits();
-		FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+		this.maSession = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
 		//ajout de la liste de tous les produits dans la session
 		maSession.setAttribute("prodListe", listeProduits);
 	}
@@ -68,10 +74,45 @@ public class ProduitManagedBean implements Serializable{
 	public void setMaSession(HttpSession maSession) {
 		this.maSession = maSession;
 	}
+
+	public UploadedFile getUf() {
+		return uf;
+	}
+
+	public void setUf(UploadedFile uf) {
+		this.uf = uf;
+	}
 	
+	public Categorie getCategorie() {
+		return categorie;
+	}
+
+	public void setCategorie(Categorie categorie) {
+		this.categorie = categorie;
+	}
+
 	//Methodes metiers
-	
 	//ajouter un produit dans la BDD
-	
+	public String ajouterProduit(){
+		produit.setPhotoProd(this.uf.getContents());
+		Produit prodAjout = produitService.addProduit(produit, categorie);
+		
+		if (prodAjout.getId() != 0) {
+
+			// recuperer la liste de clients
+			List<Produit> liste = produitService.getAllProduits();
+
+			// metre a jour la liste dans la session
+			maSession.setAttribute("prodListe", liste);
+			
+			return "gestionProduits";
+		} else {
+
+			// le message en cas dechec
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("l'ajout de produit est echoue"));
+			return "ajouterProduit";
+
+		}
+	}
 	
 }
