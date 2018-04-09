e00140f26eb42fd76c71c7544e340d530cfafe06
inti0423 <inti0423@DESKTOP-U3KTBH5>
2018-04-09 14:40:12 +0200

    gwe: package test

---
+++ eShop/src/main/java/fr/adaming/test/ProduitTest.java
@@ -0,0 +1,17 @@
+package fr.adaming.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ProduitTest {
+
+	public static void main(String[] args) {
+		// recuperer le contexte Spring IoC
+		ApplicationContext cxt = new ClassPathXmlApplicationContext("application-context.xml");
+		
+		//Ajouter un produit
+		
+
+	}
+
+}
